1d741376bdd815704a55e766d8d340d8ceeea69d
Mohammad Omar1998 <mob1998mob@example.com>
2020-03-27 19:53:32 +0200

    asimint4

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,9 +15,21 @@ class TaskController extends Controller
     }
 
     public function show($id){
-        $task1=DB::table(tasks)->find($id);
+        $task1=DB::table(task)->find($id);
 
         return view('task.show',compact('task1'));
 
     }
+public function store(Request $Request){
+       // dd($request);
+        DB::table('task')->insert([
+        'name'=>$request->name,
+        'created_at' => now(),
+        'updeted_at' => now(),
+        ]);
+
+        return redirect()->bake();
 }
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', function () {
-    return view('child');
-
-});
-
-Route::get('task','TaskController@index');
+Route::get('/','TaskController@index');
 Route::get('task1/{id}','TaskController@show');
+
+Route::post('store','TaskController@store');
+ 
