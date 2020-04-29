c1725497fc01b7a8c458932dab3b8b597c4a8e2a
Mohammad Omar1998 <mob1998mob@example.com>
2020-04-29 14:55:08 +0300

    newtasklist

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 Route::get('/','TaskController@index');
 Route::get('task/{id}','TaskController@show');
 
-Route::post('store','TaskController@store');
+Route::post('store ToTaskController','TaskController@store')->name('store');
 Route::delete('delete/{id}','TaskController@destroy');
 
 Route::put('edit/{id}','TaskController@edit');
